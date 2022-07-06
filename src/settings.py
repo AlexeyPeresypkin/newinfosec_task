eccab1cd4948e46f4ce6327ed718d232d4e60e16
Alexey Peresypkin <aepre3@gmail.com>
2022-07-06 15:19:03 +0300

    Refactor with templates

--- src/settings.py
+++ src/settings.py
@@ -127,3 +127,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = "/auth/login/"
 LOGIN_REDIRECT_URL = "index"
+LOGOUT_REDIRECT_URL = "index"
--- users/urls.py
+++ users/urls.py
@@ -3,11 +3,7 @@ from . import views
 from .views import ResourceView
 
 urlpatterns = [
-    path(
-        '',
-        ResourceView.as_view(),
-        name='index'
-    ),
+    path('', ResourceView.as_view(), name='index'),
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('create/', views.ResourceCreate.as_view(), name='create'),
     path('edit/<int:pk>', views.ResourceEdit.as_view(), name='edit'),
